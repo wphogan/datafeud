a564240651bc8e842686cec4664b284073a702c6
wphogan <hogan.wp@gmail.com>
2015-08-19 23:43:02 +0000

    Redo Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20150805205434) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "city"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "password_digest"
