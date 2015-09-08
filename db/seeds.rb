8d43e6547a9f3208f75dd435d2353780d913647d
wphogan <hogan.wp@gmail.com>
2015-09-08 17:55:14 +0000

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,11 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "DataCenter",
+             email: "hogan.wp@gmail.com",
+             password:              "qwerty",
+             password_confirmation: "qwerty",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
              
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               city: city,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
 
 users = User.order(:created_at).take(6)
 50.times do
