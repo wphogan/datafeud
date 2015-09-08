671834f84914832b85861a3c140a23b6b3030699
wphogan <hogan.wp@gmail.com>
2015-09-08 17:48:07 +0000

    re-config action mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@example.com"
+  default from: "info@datacenter.org"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
 
   def account_activation(user)
     @user = user
-   mail to: user.email, from: "noreply@example.com", subject: "Account activation"
+   mail to: user.email, from: "info@datacenter.org", subject: "Account activation"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email,  from: "noreply@example.com", subject: "Password reset"
+    mail to: user.email,  from: "info@datacenter.org", subject: "Password reset"
   end
 end
