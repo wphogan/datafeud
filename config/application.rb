034d36aa1744b305fd43f047c6733a420ce7c452
wphogan <hogan.wp@gmail.com>
2015-09-09 23:49:02 +0000

    .....

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SampleApp
+module DataFeud
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
