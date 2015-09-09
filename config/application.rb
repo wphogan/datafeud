f7b9536b938630ad23a9defe67b7b71174dfc92a
wphogan <hogan.wp@gmail.com>
2015-09-09 22:50:20 +0000

    ....

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+# config/application.rb
+config.assets.initialize_on_precompile = false
 module DataFeud
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
