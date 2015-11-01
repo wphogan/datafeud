2e2714e43838aefa0001662f70c06edf544a0cd3
wphogan <hogan.wp@gmail.com>
2015-11-01 22:26:27 +0000

    its working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,25 @@ User.create!(name:  "DataCenter",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+
+User.create!(name:  "DataCenter",
+             email: "info@datacenter.org",
+             password:              "l00selips",
+             password_confirmation: "l00selips",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
              
 
-users = User.order(:created_at).take(6)
-50.times do
-  game_name = Faker::Lorem.sentence(5)
-  issue_area = "Here's issue area"
-  summary = "Here's summary"
-  users.each { |user| user.games.create!(game_name: game_name, issue_area: issue_area, summary: summary) }
-  games = Game.order(:created_at).take(6)
-  5.times do
-    content = ((0...8).map { (65 + rand(26)).chr }.join)
-    games.each { |game| game.questions.create!(content: content) }
-  end
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   game_name = Faker::Lorem.sentence(5)
+#   issue_area = "Here's issue area"
+#   summary = "Here's summary"
+#   users.each { |user| user.games.create!(game_name: game_name, issue_area: issue_area, summary: summary) }
+#   games = Game.order(:created_at).take(6)
+#   5.times do
+#     content = ((0...8).map { (65 + rand(26)).chr }.join)
+#     games.each { |game| game.questions.create!(content: content) }
+#   end
+# end
